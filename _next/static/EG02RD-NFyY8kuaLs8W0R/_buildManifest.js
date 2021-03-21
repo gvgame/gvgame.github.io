51727b6018dc6899a5629117891aed5d026b360b
gvgame <gjvera@asu.edu>
2021-03-21 05:47:44 +0000

    Deploying to gh-pages from  @ 945c816d140701989f4859a9fc8be6a5d906b779 🚀

---
+++ _next/static/EG02RD-NFyY8kuaLs8W0R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F77dda2a6bfecdb10fe63.css","static\u002Fchunks\u002Fpages\u002Findex-47f7709b4d1c8d99cc52.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c36be019983307b4a8c0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
