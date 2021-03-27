3887771802a7ab40ff5565cc917d54f1b3c5cf5f
gvgame <gvgame@gmail.com>
2021-03-26 23:54:26 -0700

    Adding synthwave colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ee7af848e42b4bdaa28";
+/******/ 	var hotCurrentHash = "20bf4ad631c24291c6bd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
